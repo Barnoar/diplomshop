40d7f026947b4651b9b457d16c4a28352381f710
Даня Петряков <danya030697@mail.ru>
2023-06-18 16:51:16 +0500

    alpaca

---
+++ alpaca/start_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#coproc ./chat 
+#nc -lk -p 9876 >&"${COPROC[1]}" <&"${COPROC[0]}" 
+#ncat -lk -p 9876 | tee ~/chat_pipe | ./chat | tee ~/chat_pipe
+
+#ncat -lnkp 9876 -e ./chat
+
+rm -rf /tmp/f
+mkfifo /tmp/f
+
+cat /tmp/f | ./chat | nc -lk 9876 > /tmp/f
